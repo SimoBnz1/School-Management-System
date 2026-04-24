8b476ed97998bb9abf8dfb6ab1d04355f81b4e88
mohamed_bnz <simobnz81@gmail.com>
2026-04-24 10:29:46 +0100

    ajouter une if conditon pour tester la methode POST si vide ou nom

--- scripts/register_process.php
+++ scripts/register_process.php
@@ -31,3 +31,35 @@ function addUser($conn, $firstname, $lastname, $email, $password)
         echo $e->getMessage();
     }
 }
+
+if (isset($_POST['singup'])) {
+   
+    $firstname = htmlspecialchars($_POST['firstName']);
+    $lastname = htmlspecialchars($_POST['lastName']);
+    $email = htmlspecialchars(trim($_POST['email']));
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: register.php?error=fomat unvalidate");
+        exit();
+    }
+
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $confpassword = $_POST['confirmPassword'];
+
+    if ($_POST['password'] !== $confpassword) {
+        header("Location:../public/register.php?err=passM");
+        exit();
+    }
+
+    if (checkemail($conn, $email)) {
+        header("Location:../public/login.php?err=exist");
+        exit();
+    }
+    if (!empty($firstname) && !empty($lastname && !empty($email) && !empty($password) && !empty($confpassword))) {
+        addUser($conn, $firstname, $lastname, $email, $password);
+        $_SESSION['firstname']=$firstname;
+        $_SESSION['lastname']=$lastname;
+        header("Location:../public/login.php");
+    } else {
+        header("Location:../public/register.php?err=champVid");
+    }
+}
