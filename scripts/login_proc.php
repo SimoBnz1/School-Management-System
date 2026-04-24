545fa142da45026f410e13c2c62474e80524159b
mohamed_bnz <simobnz81@gmail.com>
2026-04-24 10:33:51 +0100

    Création de la fonction getPassword pour vérifier les identifiants utilisateur

--- scripts/login_proc.php
+++ scripts/login_proc.php
@@ -2,3 +2,23 @@ login_logique
 <?php
 session_start();
 require '../scripts/connection.php';
+function getPassword($email, $password, $conn)
+{
+    try {
+        $sql = "SELECT * FROM users WHERE email=:email";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(["email" => $email]);
+        $user = $stmt->fetch();
+        if ($user) {
+            if (password_verify($password, $user['password'])) {
+                return $user;
+            } else {
+                return false;
+            };
+        } else {
+            return false;
+        };
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+};
