106964a3e9bb17f28eb21fbdd9c14f86e1ee388d
sarafasra <sarafasraoui75@gmail.com>
2026-04-29 17:39:56 +0100

    ajouter students

--- prof/students.php
+++ prof/students.php
@@ -1,3 +1,61 @@
 <?php
-include "./aside.php";
+require_once "../scripts/connection.php";
+
+
+
+$stmt = $conn->prepare("
+    SELECT 
+        courses.id,
+        courses.title,
+        courses.total_hours,
+        COUNT(enrollments.student_id) AS total_students,
+        GROUP_CONCAT(users.firstname) AS firstnames,
+        GROUP_CONCAT(users.lastname) AS lastnames
+    FROM courses
+    LEFT JOIN enrollments 
+        ON courses.id = enrollments.course_id
+    LEFT JOIN users 
+        ON users.id = enrollments.student_id
+    GROUP BY courses.id, courses.title, courses.total_hours
+");
+
+$stmt->execute();
+$students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
+
+ <h2>My Courses</h2>
+
+<table class="min-w-full bg-gray-900 text-white rounded-xl overflow-hidden">
+    <thead class="bg-gray-800 text-gray-300 text-sm uppercase">
+        <tr>
+            <th class="py-3 px-4 text-left">ID</th>
+            <th class="py-3 px-4 text-left">Title</th>
+            <th class="py-3 px-4 text-left">Hours</th>
+            <th class="py-3 px-4 text-left">Students</th>
+             <th class="py-3 px-4 text-left">firstname</th>
+            <th class="py-3 px-4 text-left">lastname</th>
+        </tr>
+    </thead>
+
+    <tbody class="divide-y divide-gray-700">
+        <?php foreach($students as $student){ ?>
+            <tr class="hover:bg-gray-800 transition">
+                <td class="py-3 px-4"><?= $student['id'] ?></td>
+                <td class="py-3 px-4"><?= htmlspecialchars($student['title']) ?></td>
+                
+                
+                <td class="py-3 px-4"><?= $student['total_hours'] ?></td>
+<td class="py-3 px-4"><?= $student['total_students'] ?></td>
+<td><?= $student['firstnames'] ?></td>
+    <td><?= $student['lastnames'] ?></td>
+                <!-- <td class="py-3 px-4">
+                    <a href="course_students.php?course_id=<?= $course['id'] ?>" 
+                       class="bg-blue-600 px-3 py-1 rounded text-white hover:bg-blue-700">
+                        View Students
+                    </a>
+                </td> -->
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
